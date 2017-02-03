f832acc1e8016d13e8bbb700e727420b3bc8e0d6
Jonathan Taylor <jon.a.taylor@outlook.com>
2017-02-03 17:42:08 -0500

    Create routeCANFrame method for DataProcessor

--- AndroidCarDashhboard/DataProcessor.cpp
+++ AndroidCarDashhboard/DataProcessor.cpp
@@ -4,3 +4,70 @@ DataProcessor::DataProcessor()
 {
 
 }
+
+DataProcessor::routeCANFrame(QCanBusFrame frame)
+{
+    int id = frame.frameId();
+    QByteArray data = frame.payload();
+
+    // For efficienty, these cases should be ordered with the most
+    // frequent ids at the top.
+    switch(id)
+    {
+    case TACHOMETER_ID:
+        updateGroundSpeed(data);
+        break;
+    case PITOT_ID:
+        updateAirSpeed(data);
+        break;
+    case EFI_PRESSURE_ID:
+        updateEFIPressure(data);
+        break;
+    case MEGASQUIRT_ID:
+        updateMegasquirt(data);
+        break;
+    case CURRENT_ID:
+        updateCurrent(data);
+        break;
+    case VOLTAGE_ID:
+        updateVoltage(data);
+        break;
+    default:
+        // Do nothing if the CAN frame does not have an ID that we care about
+    }
+}
+
+DataProcessor::updateGroundSpeed(QByteArray data)
+{
+
+}
+
+DataProcessor::updateAirSpeed(QByteArray data)
+{
+
+}
+
+DataProcessor::updateEFIPressure(QByteArray data)
+{
+
+}
+
+DataProcessor::updateMegasquirt(QByteArray data)
+{
+
+}
+
+DataProcessor::updateCurrent(QByteArray data)
+{
+
+}
+
+DataProcessor::updateVoltage(QByteArray data)
+{
+
+}
+
+DataProcessor::updateInterfaceIndicators(QByteArray data)
+{
+
+}
--- AndroidCarDashhboard/DataProcessor.h
+++ AndroidCarDashhboard/DataProcessor.h
@@ -1,11 +1,33 @@
 #ifndef DATAPROCESSOR_H
 #define DATAPROCESSOR_H
 
+#include <QCanBusFrame>
+
+
 
 class DataProcessor
 {
 public:
     DataProcessor();
+
+    void routeCANFrame(QCanBusFrame frame);
+
+    // Setters for values that come from CANInterface
+    void updateGroundSpeed(QByteArray data);
+    void updateAirSpeed(QByteArray data);
+    void updateEFIPressure(QByteArray data);
+    void updateMegasquirt(QByteArray data);
+    void updateCurrent(QByteArray data);
+    void updateVoltage(QByteArray data);
+
+private:
+    // CAN frame ids. These numbers have not been finalized or verified.
+    int const TACHOMETER_ID = 1584; // Used for ground speed
+    int const PITOT_ID = 1648; // Used for air speed
+    int const EFI_PRESSURE_ID = 0; // Used for fuel injection info
+    int const MEGASQUIRT_ID = 0; // Used for engine temperature and other info
+    int const CURRENT_ID = 0;
+    int const VOLTAGE_ID = 0;
 };
 
 #endif // DATAPROCESSOR_H
