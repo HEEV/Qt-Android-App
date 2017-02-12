0ca68de58d23f6dc81d359ff56b87248f37de3d6
Samuel Ellicott <nebk2010@gmail.com>
2017-02-11 19:29:44 -0500

    Added CanNodeParser Class
    This is a class to change a QByteArray into a integer type as defined in
    stdint.h. They are a bunch of overloaded getData functions that take a
    QByteArray and a integer varible of the desired type. They will return
    INVALID_DATA on an unsuccessfull return.

---
+++ AndroidCarDashhboard/CanNodeParser.h
@@ -0,0 +1,52 @@
+/**
+ * Convert from a CanNode message frame to basic integer types
+ *
+ * Sam Ellicott: 2-11-17
+ */
+
+#ifndef CAN_NODE_PARSER_H_
+#define CAN_NODE_PARSER_H_
+
+#include <QCanBusFrame>
+#include <UIRaceDataset.h>
+#include <QDebug>
+#include <stdint.h>
+
+class CanNodeParser
+{
+public:
+    //no constructor needed just a bunch of static functions
+
+
+    enum CanParserError {
+        DATA_OK=0,
+        INVALID_TYPE
+    }
+
+    //begin static functions
+    /// \brief Get a signed 8-bit integer from a CanMessage.
+    static CanParserError getData(const QByteArray& msg,   int8_t& data);
+    /// \brief Get an unsigned 8-bit integer from a CanMessage.
+    static CanParserError getData(const QByteArray& msg,  uint8_t& data);
+    /// \brief Get a signed 16-bit integer from a CanMessage.
+    static CanParserError getData(const QByteArray& msg,  int16_t& data);
+    /// \brief Get an unsigned 16-bit integer from a CanMessage.
+    static CanParserError getData(const QByteArray& msg, uint16_t& data);
+    /// \brief Get a signed 32-bit integer from a CanMessage.
+    static CanParserError getData(const QByteArray& msg,  int32_t& data);
+    /// \brief Get an unsigned 32-bit integer from a CanMessage.
+    static CanParserError getData(const QByteArray& msg, uint32_t& data);
+
+private:
+   enum CanNodeDataType{
+        CAN_UINT8,     ///< Unsigned 8-bit integer
+        CAN_INT8,      ///< Signed 8-bit integer
+        CAN_UINT16,    ///< Unsigned 16-bit integer
+        CAN_INT16,     ///< Signed 16-bit integer
+        CAN_UINT32,    ///< Unsigned 32-bit integer
+        CAN_INT32,     ///< Signed 32-bit integer
+        CAN_BIT_FIELD, ///< Each bit defines the state of value
+        CAN_CUSTOM     ///< Catch all
+    };
+}
+#endif //CAN_NODE_PARSER_H_
