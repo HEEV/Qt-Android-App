f533179098c3cba77965aa260c6b73688e054ba3
Samuel Ellicott <nebk2010@gmail.com>
2017-02-11 19:48:37 -0500

    Fixed errors in code

--- AndroidCarDashhboard/CanNodeParser.cpp
+++ AndroidCarDashhboard/CanNodeParser.cpp
@@ -6,93 +6,93 @@
 
 //begin static functions
 /// \brief Get a signed 8-bit integer from a CanMessage.
-CanParserError CanNodeParser::getData(const QByteArray& msg, int8_t& data)
+int CanNodeParser::getData(const QByteArray msg, int8_t& data)
 {
     //check configuration byte
-    if((msg->data[0] >> 5) != CAN_INT8 ||   //not right type
-       (msg->data[0] & 0x1F) != CAN_DATA ){ //not data
+    if((msg[0] >> 5) != CAN_INT8 ||   //not right type
+       (msg[0] & 0x1F) != CAN_DATA ){ //not data
 
         return INVALID_TYPE;
     }
 
-    data = (int8_t) data[1];
+    data = (int8_t) msg[1];
     return DATA_OK;
 }
 
 /// \brief Get an unsigned 8-bit integer from a CanMessage.
-CanParserError CanNodeParser::getData(const QByteArray& msg, uint8_t& data)
+int CanNodeParser::getData(const QByteArray msg, uint8_t& data)
 {
     //check configuration byte
-    if((msg->data[0] >> 5) != CAN_UINT8 ||   //not right type
-       (msg->data[0] & 0x1F) != CAN_DATA ){ //not data
+    if((msg[0] >> 5) != CAN_UINT8 ||   //not right type
+       (msg[0] & 0x1F) != CAN_DATA ){ //not data
 
         return INVALID_TYPE;
     }
 
-    data = (uint8_t) data[1];
+    data = (uint8_t) msg[1];
     return DATA_OK;
 }
 
 /// \brief Get a signed 16-bit integer from a CanMessage.
-CanParserError CanNodeParser::getData(const QByteArray& msg, int16_t& data)
+int CanNodeParser::getData(const QByteArray msg, int16_t& data)
 {
     //check configuration byte
-    if((msg->data[0] >> 5) != CAN_INT16 ||   //not right type
-       (msg->data[0] & 0x1F) != CAN_DATA ){ //not data
+    if((msg[0] >> 5) != CAN_INT16 ||   //not right type
+       (msg[0] & 0x1F) != CAN_DATA ){ //not data
 
         return INVALID_TYPE;
     }
 
-    data  = (int16_t) data[1];
-    data |= (int16_t) (data[2]<<8);
+    data  = (int16_t) msg[1];
+    data |= (int16_t) (msg[2]<<8);
     return DATA_OK;
 }
 
 /// \brief Get an unsigned 16-bit integer from a CanMessage.
-CanParserError CanNodeParser::getData(const QByteArray& msg, uint16_t& data)
+int CanNodeParser::getData(const QByteArray msg, uint16_t& data)
 {
     //check configuration byte
-    if((msg->data[0] >> 5) != CAN_UINT16 ||   //not right type
-       (msg->data[0] & 0x1F) != CAN_DATA ){ //not data
+    if((msg[0] >> 5) != CAN_UINT16 ||   //not right type
+       (msg[0] & 0x1F) != CAN_DATA ){ //not data
 
         return INVALID_TYPE;
     }
 
-    data  = (uint16_t) data[1];
-    data |= (uint16_t) (data[2]<<8);
+    data  = (uint16_t) msg[1];
+    data |= (uint16_t) (msg[2]<<8);
     return DATA_OK;
 }
 
 /// \brief Get a signed 32-bit integer from a CanMessage.
-CanParserError CanNodeParser::getData(const QByteArray& msg, int32_t& data)
+int CanNodeParser::getData(const QByteArray msg, int32_t& data)
 {
     //check configuration byte
-    if((msg->data[0] >> 5) != CAN_INT32 ||   //not right type
-       (msg->data[0] & 0x1F) != CAN_DATA ){ //not data
+    if((msg[0] >> 5) != CAN_INT32 ||   //not right type
+       (msg[0] & 0x1F) != CAN_DATA ){ //not data
 
         return INVALID_TYPE;
     }
 
-    data  = (int32_t) data[1];
-    data |= (int32_t) (data[2]<<8);
-    data |= (int32_t) (data[3]<<16);
-    data |= (int32_t) (data[4]<<24);
+    data  = (int32_t) msg[1];
+    data |= (int32_t) (msg[2]<<8);
+    data |= (int32_t) (msg[3]<<16);
+    data |= (int32_t) (msg[4]<<24);
     return DATA_OK;
 }
 
 /// \brief Get an unsigned 32-bit integer from a CanMessage.
-CanParserError CanNodeParser::getData(const QByteArray& msg, uint32_t& data)
+int CanNodeParser::getData(const QByteArray msg, uint32_t& data)
 {
     //check configuration byte
-    if((msg->data[0] >> 5) != CAN_UINT32 ||   //not right type
-       (msg->data[0] & 0x1F) != CAN_DATA ){ //not data
+    if((msg[0] >> 5) != CAN_UINT32 ||   //not right type
+       (msg[0] & 0x1F) != CAN_DATA ){ //not data
 
         return INVALID_TYPE;
     }
 
-    data  = (uint32_t) data[1];
-    data |= (uint32_t) (data[2]<<8);
-    data |= (uint32_t) (data[3]<<16);
-    data |= (uint32_t) (data[4]<<24);
+    data  = (uint32_t) msg[1];
+    data |= (uint32_t) (msg[2]<<8);
+    data |= (uint32_t) (msg[3]<<16);
+    data |= (uint32_t) (msg[4]<<24);
     return DATA_OK;
 }
--- AndroidCarDashhboard/CanNodeParser.h
+++ AndroidCarDashhboard/CanNodeParser.h
@@ -16,30 +16,27 @@ class CanNodeParser
 {
 public:
     //no constructor needed just a bunch of static functions
-
-
-    enum CanParserError {
-        DATA_OK=0,
-        INVALID_TYPE
-    }
+    static const int DATA_OK=0;
+    static const int INVALID_TYPE=1;
 
     //begin static functions
     /// \brief Get a signed 8-bit integer from a CanMessage.
-    static CanParserError getData(const QByteArray& msg,   int8_t& data);
+    static int getData(const QByteArray msg,   int8_t& data);
     /// \brief Get an unsigned 8-bit integer from a CanMessage.
-    static CanParserError getData(const QByteArray& msg,  uint8_t& data);
+    static int getData(const QByteArray msg,  uint8_t& data);
     /// \brief Get a signed 16-bit integer from a CanMessage.
-    static CanParserError getData(const QByteArray& msg,  int16_t& data);
+    static int getData(const QByteArray msg,  int16_t& data);
     /// \brief Get an unsigned 16-bit integer from a CanMessage.
-    static CanParserError getData(const QByteArray& msg, uint16_t& data);
+    static int getData(const QByteArray msg, uint16_t& data);
     /// \brief Get a signed 32-bit integer from a CanMessage.
-    static CanParserError getData(const QByteArray& msg,  int32_t& data);
+    static int getData(const QByteArray msg,  int32_t& data);
     /// \brief Get an unsigned 32-bit integer from a CanMessage.
-    static CanParserError getData(const QByteArray& msg, uint32_t& data);
+    static int getData(const QByteArray msg, uint32_t& data);
 
 private:
    enum CanNodeDataType{
-        CAN_UINT8,     ///< Unsigned 8-bit integer
+        CAN_DATA=0,
+        CAN_UINT8=0,     ///< Unsigned 8-bit integer
         CAN_INT8,      ///< Signed 8-bit integer
         CAN_UINT16,    ///< Unsigned 16-bit integer
         CAN_INT16,     ///< Signed 16-bit integer
@@ -48,5 +45,5 @@ private:
         CAN_BIT_FIELD, ///< Each bit defines the state of value
         CAN_CUSTOM     ///< Catch all
     };
-}
+};
 #endif //CAN_NODE_PARSER_H_
